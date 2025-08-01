75d3643ba0b9b6888ba06b5c6994d5d5327764e9
Carlos Suarez <110506367+Unusuario2@users.noreply.github.com>
2025-08-01 10:29:58 +0200

    Initial commit

---
+++ src/utils/psdwriter/psdmain.h
@@ -0,0 +1,35 @@
+//===== PSDWriter -> Written by Unusuario2, https://github.com/Unusuario2  ====//
+//
+// Purpose:
+//
+// $NoKeywords: $
+//=============================================================================//
+#ifndef PSDMAIN_H
+#define PSDMAIN_H
+
+#ifdef _WIN32
+#pragma once
+#endif // _WIN32
+
+
+#define G_RGPENDFILENAMESIZE		14u
+#define G_RGPASSETCONVERTLISTSIZE	10u
+
+
+extern bool			g_bIsSingleFile;
+extern bool			g_bSignature;
+extern bool			g_bTemplateGeneration;
+extern bool			g_bDeleteSource;
+extern float		g_fGlobalTimer;
+extern uint8_t		g_uiCompressionType;
+extern uint8_t      g_uiForceImageBit;
+extern Color		header_color;
+extern Color		successful_color;
+extern Color		failed;
+extern char			g_szSignature[1][128];
+extern char			g_szGameMaterialSrcDir[MAX_PATH];
+extern const char*	g_rgpAssetConvertList[G_RGPASSETCONVERTLISTSIZE];
+extern const char*	g_rgpEndFileName[G_RGPENDFILENAMESIZE];
+
+
+#endif // PSDMAIN_H
---
+++ src/utils/psdwriter/psdwriter.h
@@ -0,0 +1,31 @@
+//===== PSDWriter -> Written by Unusuario2, https://github.com/Unusuario2  ====//
+//
+// Purpose:
+//
+// $NoKeywords: $
+//=============================================================================//
+#ifndef PSDWRITER_H
+#define PSDWRITER_H
+
+#ifdef _WIN32
+#pragma once
+#endif // _WIN32
+
+#define MATERIALSRC_DIR		"materialsrc"
+
+//-----------------------------------------------------------------------------
+// Purpose: Basic image definitions
+//-----------------------------------------------------------------------------
+#define IMAGE8BITS_ID				8u
+#define IMAGE16BITS_ID				16u
+#define IMAGE32BITS_ID				32u
+#define MAX_WIGHT_RES_X				4096u
+#define MAX_WIGHT_RES_Y				MAX_WIGHT_RES_X
+#define MAX_IMAGE_CHANNELS			4u
+#define TEMAPLATE_COFIG_NAME_BASE	"template"		// e.g: Template_color, template_normal, etc...
+
+
+void CheckAndExportToPsd(const char* pFileName);
+
+
+#endif // PSDWRITER_H
